d59024b62dfefc1ae280e4a4c7f7780c55b2c564
Nikhita Madhanpall <nikhita@astro4dev.org>
2021-05-04 22:57:05 +0200

    Initial Commit

---
+++ Tutorial1/twitter_credentials.py
@@ -0,0 +1,8 @@
+
+# Variables which contain the user credentials needed to access the Twitter API
+# Copy and paste your credentials within the quotation marks '' as these are string variables
+
+api_key = ''
+api_secret_key =  ''
+access_token = ''
+access_token_secret = ''
